035ce9ce371d21cde4d2a70054334fb6d556e796
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-17 19:11:44 +0100

    added route to get on /things endpoint

--- server/routes/thingsRoutes.js
+++ server/routes/thingsRoutes.js
@@ -3,4 +3,9 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/things", async (req, res) => {
+  const things = await Thing.find();
+  res.json({ things });
+});
+
 module.exports = router;
