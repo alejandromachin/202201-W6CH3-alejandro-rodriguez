4021d17ac4b5e94fbfef18f4f86b2440bc8ea852
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-17 18:44:11 +0100

    added data to enviroment and called the function connectdatabase

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("thingsIAlreadyKnow:root");
 
+const connectToDatabase = require("./database");
 const initializeServer = require("./server");
 
 const port = process.env.SERVER_PORT || 3001;
+const mongoConnectionString = process.env.MONGO_STRING;
 
 (async () => {
   try {
+    await connectToDatabase(mongoConnectionString);
     await initializeServer(port);
     debug("Estamos activos");
   } catch (error) {
