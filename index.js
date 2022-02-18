28ff44302ca3da88228b420aaed77b9d02a3c98d
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-18 08:07:19 +0100

    added conditions errors if the db is not editable

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ require("dotenv").config();
 const debug = require("debug")("thingsIAlreadyKnow:root");
 
 const connectToDatabase = require("./database");
+const getUserChoices = require("./parameters/userParameters");
 const initializeServer = require("./server");
-
-const port = process.env.SERVER_PORT || 3001;
 
 const mongoConnectionString = process.env.MONGO_STRING;
 
 (async () => {
   try {
+    const { userPort, dataBaseOption, editable } = await getUserChoices();
     await connectToDatabase(mongoConnectionString);
-    await initializeServer(port);
-    debug("Estamos activos");
+    await initializeServer(userPort);
+    debug(`Server listening on http://localhost:${userPort}`);
+
+    module.exports = { userPort, dataBaseOption, editable };
   } catch (error) {
     debug(`Error: ${error.message}`);
   }
--- server/routes/thingsRoutes.js
+++ server/routes/thingsRoutes.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const Thing = require("../../database/models/Thing");
+const editable = require("../../index");
 
 const router = express.Router();
 
@@ -14,24 +15,49 @@ router.get("/things/:id", async (req, res) => {
   const things = await Thing.findById(id);
   res.json({ things });
 });
+
 router.delete("/things/:id", async (req, res) => {
-  const { id } = req.params;
-  const things = await Thing.findByIdAndDelete(id);
-  res.json({ things });
+  res.status(404);
+  res.json({
+    error: true,
+    message: "Sorry, you are not alowed to do this.",
+  });
 });
-
 router.post("/thing", async (req, res) => {
-  const newThing = req.body;
-
-  const createdThing = await Thing.create(newThing);
-  res.status(201);
-  res.json(createdThing);
+  res.status(404);
+  res.json({
+    error: true,
+    message: "Sorry, you are not alowed to do this.",
+  });
 });
 router.put("/thing/:id", async (req, res) => {
-  const { id } = req.params;
-  const newThing = req.body;
-  const createdThing = await Thing.findByIdAndUpdate(id, newThing);
-  res.status(201);
-  res.json(createdThing);
+  res.status(404);
+  res.json({
+    error: true,
+    message: "Sorry, you are not alowed to do this.",
+  });
 });
+
+if (editable) {
+  router.delete("/things/:id", async (req, res) => {
+    const { id } = req.params;
+    const things = await Thing.findByIdAndDelete(id);
+    res.json({ things });
+  });
+
+  router.post("/thing", async (req, res) => {
+    const newThing = req.body;
+
+    const createdThing = await Thing.create(newThing);
+    res.status(201);
+    res.json(createdThing);
+  });
+  router.put("/thing/:id", async (req, res) => {
+    const { id } = req.params;
+    const newThing = req.body;
+    const createdThing = await Thing.findByIdAndUpdate(id, newThing);
+    res.status(201);
+    res.json(createdThing);
+  });
+}
 module.exports = router;
